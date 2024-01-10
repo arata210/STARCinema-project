251d95a4e848b5575f0e6f902e21196ed6b7d4b4
Xroo <sl20113419@gmail.com>
2024-01-10 21:31:11 +0800

    20240110

--- connect_dbs.py
+++ connect_dbs.py
@@ -9,6 +9,98 @@ class MongoDBConn:
         # 获取数据库的连接
         self.client = MongoClient('192.168.88.130', 27017)
 
+    # 电影
+    def find_last_id(self, col):
+        cinemadb = self.client["CinemaDB"]
+        film = cinemadb[col]
+        if film.count_documents({}) == 0:
+            return 1
+        documents = film.find().sort({"_id": -1}).limit(1)
+        for document in documents:
+            return int(document["_id"]) + 1
+
+
+    def getColl(self):
+        cinemadb = self.client["CinemaDB"]
+        collections = cinemadb.list_collection_names()
+        print(collections)
+
+    def findById(self, film_id):
+        cinemadb = self.client["CinemaDB"]
+        film = cinemadb["films"]
+        return film.find_one({"movie_id": film_id})
+
+    def findAll(self):
+        cinemadb = self.client["CinemaDB"]
+        film = cinemadb["films"]
+        return list(film.find())
+
+    def delById(self, film_id):
+        cinemadb = self.client["CinemaDB"]
+        film = cinemadb["films"]
+        result = film.delete_one({"movie_id": film_id})
+        if result.deleted_count == 1:
+            return f"成功删除电影，movie_id: {film_id}"
+        else:
+            return f"未找到电影，movie_id: {film_id}"
+
+
+
+    def updateFilm(self, movie_id, movie_intro):
+        cinemadb = self.client["CinemaDB"]
+        film = cinemadb["films"]
+        try:
+            film.update_one({"movie_id": movie_id}, {"$set": {"movie_intro": movie_intro}})
+            return "更新成功！"
+        except Exception as e:
+            return f"更新失败，发生错误：{e}"
+
+    def filmName(self, film_id):
+        cinemadb = self.client["CinemaDB"]
+        film = cinemadb["films"]
+        return film.find_one({"movie_id": film_id})["move_name"]
+
+    # 场次
+
+    def addSession(self, film_id, language, date, start_time, length, hall, type, price):
+        cinemadb = self.client["CinemaDB"]
+        col = "session" + self.filmName(film_id)
+        session = cinemadb[col]
+        # 将字符串时间转换为 datetime 对象
+        start_time2 = datetime.strptime(start_time, "%H:%M")
+        # 创建表示时长的 timedelta 对象
+        duration = timedelta(minutes=length)
+        # 计算结束时间
+        finish_time = (start_time2 + duration).strftime('%H:%M')
+        try:
+            session.insert_one({"_id": self.find_last_id(col),
+                                "movie_id": film_id,
+                                "date": date,
+                                "start_time": start_time,
+                                "hall": hall,
+                                "length": length,
+                                "language": language,
+                                "type": type,
+                                "price": price,
+                                "finish_time": finish_time
+                                })
+            return "插入成功！"
+        except Exception as e:
+            return f"插入失败，发生错误：{e}"
+
+    def findSession(self, film_id):
+        cinemadb = self.client["CinemaDB"]
+        col = "session" + self.filmName(film_id)
+        session = cinemadb[col]
+        return  session.find_one({"movie_id": film_id})
+
+    def findSessionById(self, film_id, session_id):
+        cinemadb = self.client["CinemaDB"]
+        col = "session" + self.filmName(film_id)
+        session = cinemadb[col]
+        return session.find_one({"_id": session_id})
+
+
 
 class RedisConn:
     # 初始化 Redis 连接
@@ -17,7 +109,10 @@ class RedisConn:
 
 
 class MySQLConn:
-    # 初始化 mysql 连接
+    """
+    MySQL操作, 连接/关闭/增/删/改/查
+    """
+    # 初始化 MySQL 连接
     def __init__(self, host='localhost', user='root', password='123456', database='cinema'):
         # 初始化数据库连接
         self.conn = mysql.connector.connect(
@@ -34,12 +129,70 @@ class MySQLConn:
         self.cursor.close()
         self.conn.close()
 
-    def execute_query(self, query):
+    def execute_query(self, query, params=None):
         # 执行查询
-        self.cursor.execute(query)
+        if params is not None:
+            self.cursor.execute(query, params)
+        else:
+            self.cursor.execute(query)
         # 获取查询结果
         result = self.cursor.fetchall()
+        # 结果为空返回None
+        if len(result) == 0:
+            return None
+        # 返回查询结果
         return result
+
+    def execute_insert(self, query, data):
+        try:
+            # 执行插入操作
+            self.cursor.execute(query, data)
+            # 提交事务
+            self.conn.commit()
+            # 返回插入的记录的ID
+            return self.cursor.lastrowid
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            # 发生错误时回滚事务
+            self.conn.rollback()
+            return None
+
+    def execute_delete(self, query, data=None):
+        try:
+            # 执行删除操作
+            self.cursor.execute(query, data)
+            # 提交事务
+            self.conn.commit()
+            # 返回受影响的行数
+            return self.cursor.rowcount
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            # 发生错误时回滚事务
+            self.conn.rollback()
+            return 0
+
+    def execute_update(self, query, data):
+        try:
+            # 执行更新操作
+            self.cursor.execute(query, data)
+            # 提交事务
+            self.conn.commit()
+            # 返回受影响的行数
+            return self.cursor.rowcount
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            # 发生错误时回滚事务
+            self.conn.rollback()
+            return 0
+
+    # def add_film_in_mysql(self):
+    #     film_info = MySQLConn()
+    #     insert_query = "INSERT INTO film (movie_id, title, release_date, country, length, director, genre, actor) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    #     insert_data = (
+    #     "30270746", "寒单", "2024-01-05", "中国台湾", "125", "黄朝亮", "剧情", "郑人硕 / 胡宇威 / 黄瀞怡 / 林予晞")
+    #     inserted_id = film_info.execute_insert(insert_query, insert_data)
+    #     print(f"Inserted record ID: {inserted_id}")
+    #     film_info.close_conn()
 
 #
 # Order = OrderManager("localhost", "root", "123456", "FilmOrder")
--- flask_test.py
+++ flask_test.py
@@ -1,23 +1,94 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from create_coupon import Get_all_info
-from film import findAll
-from film_info import find_film
-from douban_rating_nums import douban_rating_num
+from index_page import find_films_info, update_films_rating_num, find_films_intro, make_html_li
+from info_page import find_film_info_byid, find_film_intro_byid
+from session_page import find_film_session_byid, find_film_cover_byid, make_html_li_2
 
-app = Flask(__name__)
+# 设置静态元素存放路径和名字
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+# 启动前更新电影评分
+# update_films_rating_num()
 
 
 @app.route('/')
 def index():
-    # 示例数据
-    a = find_film()
-    # b = douban_rating_num(a[0])
-    b = list()
-    for x in range(len(a)):
-        b.append(douban_rating_num(a[x][0]))
-    print(b)
-    return render_template("index.html", data=a, data2=b)
+    """
+    星光影院(STARCinema)首页
+    :return: html格式的电影数据
+    功能扩充：电影状态 - 1 上映中
+                - -1 结束放映
+                - 0 即将上映
+    """
+    # 获取电影基本信息
+    films_info = find_films_info()
+    # 获取电影图片信息
+    films_cover = find_films_intro()
+    # 双重循环 制作显示的信息
+    film_index = str()
+    for i in range(len(films_info)):
+        for j in range(len(films_cover)):
+            if films_info[i][0] == films_cover[j]['movie_id']:
+                film_index = film_index + (make_html_li(films_cover[j]['movie_cover'], films_info[i][0], films_info[i][1], films_info[i][5], films_info[i][-2], films_info[i][-1]))
+    # 发送数据
+    return render_template("index.html", data=film_index)
 
+
+@app.route('/info.html', methods=['GET'])
+def info():
+    """
+    单个电影信息页
+    :return: 电影数据
+    """
+    # 接收movie_id
+    the_movie_id = request.args.get('movie_id')
+    # 获取MySQL数据
+    film_info = find_film_info_byid(the_movie_id)[0]
+    title = film_info[1]
+    release = film_info[2]
+    country = film_info[3]
+    movie_length = film_info[4]
+    director = film_info[5]
+    genre = film_info[6]
+    actor = film_info[7]
+    rating_num = film_info[8]
+    # 获取MongoDB数据
+    film_intro = find_film_intro_byid(the_movie_id)
+    movie_intro = film_intro['movie_intro']
+    movie_cover = find_film_intro_byid(the_movie_id)['movie_cover']
+    # 发送数据
+    return render_template('info.html', title=title, release=release, country=country,
+                           movie_length=movie_length, director=director, genre=genre,
+                           actor=actor, rating_num=rating_num, movie_intro=movie_intro,
+                           movie_cover=movie_cover, movie_id=the_movie_id)
+
+
+@app.route('/session.html', methods=['GET'])
+def session():
+    the_movie_id = request.args.get('movie_id')
+    cover = find_film_cover_byid(the_movie_id)
+    film_info = find_film_info_byid(the_movie_id)[0]
+    title = film_info[1]
+    rating = film_info[-1]
+    film_sessions = find_film_session_byid(the_movie_id)
+    sessions = str()
+    if len(film_sessions) == 0:
+        return render_template('session.html', sessions='<li><div class="s-middle"><p>暂无排片</p></div></li>', cover=cover, title=title, rating=rating)
+    for i in range(len(film_sessions)):
+        sessions = sessions + (make_html_li_2(film_sessions[i]['start_time'], film_sessions[i]['finish_time'], film_sessions[i]['language'], film_sessions[i]['type'], film_sessions[i]['hall'], film_sessions[i]['price'], str(film_sessions[i]['_id']), str(film_sessions[i]['movie_id'])))
+
+    return render_template('session.html', sessions=sessions, cover=cover, title=title, rating=rating)
+
+
+@app.route('/login.html', methods=['GET'])
+def login():
+    data = '1'
+    return render_template('login.html', data=data)
+
+
+@app.route('/ticket.html', methods=['GET'])
+def ticket():
+    data = '1'
+    return render_template('ticket.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
